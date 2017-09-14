35cd2f738198fde35d222e455c20e23aa325632c
Hieu Nguyen <hieu.nguyen@gmail.com>
2017-09-14 23:35:20 +0700

    Init a grpc server

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,46 @@
 package main
 
-import "os/exec"
-import "os"
-import "log"
+// import "os/exec"
+// import "os"
+// import "log"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/hieunm6/say-grpc/api"
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
-	cmd := exec.Command("flite", "-t", os.Args[1], "-o", "output.wav")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+	port := flag.Int("p", 8080, "port to listen to")
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		logrus.Fatalf("could not listen to port %d: %v", *port)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterTexToSpeechServer(s, server{})
+	err = s.Serve(lis)
+	if err != nil {
+		logrus.Fatalf("could not serve: %v", err)
 	}
 }
+
+type server struct{}
+
+func (server) Say(ctx context.Context, text *pb.Text) (*pb.Speech, error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+// cmd := exec.Command("flite", "-t", os.Args[1], "-o", "output.wav")
+// cmd.Stdout = os.Stdout
+// cmd.Stderr = os.Stderr
+// if err := cmd.Run(); err != nil {
+// 	log.Fatal(err)
+// }
